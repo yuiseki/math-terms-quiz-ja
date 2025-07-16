dbe9423bc45e139a79d43a27948c07b30b842c93
yuiseki <yuiseki@gmail.com>
2025-07-16 20:48:21 +0900

    Update Vite config with base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/math-terms-quiz-ja/',
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  optimizeDeps: { exclude: ['lucide-react'] },
 });
